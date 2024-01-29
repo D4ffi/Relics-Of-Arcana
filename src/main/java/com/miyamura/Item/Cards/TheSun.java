47df5aec7b803f0c075faeb1bf303a4f85c047f7
daffi <koss.cr@outlook.com>
2024-01-29 10:33:14 -0600

    Add ability activation to TheSun card
    This update involves adding `activateAbility` and `applyEffect` methods to TheSun.java and importing relevant classes. Now, when a player activates TheSun card, it sets other entities within a certain range on fire.

--- src/main/java/com/miyamura/Item/Cards/TheSun.java
+++ src/main/java/com/miyamura/Item/Cards/TheSun.java
@@ -1,8 +1,13 @@
 package com.miyamura.Item.Cards;
 
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,5 +21,19 @@ public class TheSun extends CardManager{
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         super.appendTooltip(stack, world, tooltip, context);
         tooltip.add(Text.translatable("description.theSun"));
+    }
+    @Override
+    public  void activateAbility(PlayerEntity player){
+        Box box = createBox(player, CardManager.RANGE);
+        applyEffect(player,box);
+    }
+    @Override
+    public void applyEffect(PlayerEntity player, Box areaOfEffect){
+        List<Entity> entities = player.getWorld().getOtherEntities(player, areaOfEffect);
+        for (Entity entity : entities) {
+            if (entity instanceof LivingEntity || entity instanceof MobEntity){
+                entity.setOnFireFor(10);
+            }
+        }
     }
 }
